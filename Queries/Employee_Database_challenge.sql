ab8efcfa2c2f499c93cc2aeb9c784bfaeed07d85
Liz <103263248+liz-mcdaneld@users.noreply.github.com>
2022-07-02 08:58:00 -0500

    updating challenge code

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,3 +35,64 @@ GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
 
 SELECT * FROM retiring_titles;
+
+-- Mentorship eligibility
+ELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (t.to_date = '9999-01-01')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC, de.from_date DESC;
+
+SELECT * FROM mentorship_eligibilty;
+
+
+
+--Additional tables
+-- mentee counts
+SELECT COUNT(me.title), me.title
+INTO mentee_counts
+FROM mentorship_eligibilty AS me
+GROUP BY me.title
+ORDER BY COUNT(me.title) DESC;
+
+--filtering mentor list
+SELECT DISTINCT ON (e.emp_no)e.emp_no, 
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO unique_mentors_list
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (t.from_date BETWEEN '1985-01-01' AND '1985-12-31')
+AND (t.to_date = '9999-01-01')
+ORDER by e.emp_no ASC, t.from_date DESC;
+
+SELECT * FROM unique_mentors_list;
+
+--mentor count
+SELECT COUNT(um.title), um.title
+INTO unique_mentor_count
+FROM unique_mentors_list AS um
+GROUP BY um.title
+ORDER BY COUNT(um.title) DESC;
+
+SELECT * FROM unique_mentor_count;
+
+
+
